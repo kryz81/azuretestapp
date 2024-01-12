010ea25023447a0edeb86a38add4aeb70f039796
kryz81 <kpiechowicz@gmail.com>
2024-01-12 16:36:41 +0100

    app

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello World from app on ' + port)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
